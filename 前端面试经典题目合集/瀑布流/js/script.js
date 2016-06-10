cd4d5cd1954a5536f67617c2deb3e846f84b5ffb
youthcity <397731024@qq.com>
2016-06-10 23:13:14 +0800

    add js瀑布流（2）

--- 前端面试经典题目合集/瀑布流/js/script.js
+++ 前端面试经典题目合集/瀑布流/js/script.js
@@ -1,5 +1,24 @@
 window.onload = function(){
 	waterfall('main', 'box');
+	var dataInt = {"data":[{"src":'26.jpg'},{"src":'27.jpg'},{"src":'28.jpg'},{"src":'29.jpg'},{"src":'30.jpg'},{"src":'31.jpg'},{"src":'32.jpg'},{"src":'33.jpg'},{"src":'34.jpg'},{"src":'35.jpg'},{"src":'36.jpg'}]};
+	window.onscroll = function(){
+		if (checkScrollSlide()) {
+			var oParent = document.getElementById('main');
+			for (var i = 0; i < dataInt.data.length; i++) {
+				var oBox = document.createElement('div');
+				oBox.className = 'box';
+				oParent.appendChild(oBox);
+				var oPic = document.createElement('div');
+				oPic.className = 'pic';
+				oBox.appendChild(oPic);
+				var oImg = document.createElement('img');
+				oImg.src = "img/"+dataInt.data[i].src;
+				oPic.appendChild(oImg);
+			}
+			waterfall('main', 'box');
+		}
+
+	}
 };
 
 function waterfall(parent, box){
@@ -17,9 +36,19 @@ function waterfall(parent, box){
 	var hArr = []; //存放每一列高度
 	for (var i = 0; i < oBoxs.length; i++) {
 		if (i<cols) {
-			
+			hArr.push(oBoxs[i].offsetHeight);
+		}else {
+			var minH = Math.min.apply(null,hArr);
+			var index =  getMinhIndex(hArr, minH);
+			oBoxs[i].style.position = 'absolute';
+			oBoxs[i].style.top = minH+'px';
+			// oBoxs[i].style.left = oBoxW*index+'px'; //方法一取left值
+			oBoxs[i].style.left = oBoxs[index].offsetLeft+'px'; //方法二
+			hArr[index] += oBoxs[i].offsetHeight;
+			console.log(hArr);
 		}
 	}
+	console.log(hArr);
 }
 
 function getByClass(parent, clsName){
@@ -31,4 +60,23 @@ function getByClass(parent, clsName){
  		}
  	}
  	return boxArr;
+}
+
+function getMinhIndex(arr,val){
+	for(var i in arr){
+		if(arr[i] == val){
+			return i;
+		}
+	}
+}
+
+function checkScrollSlide(){
+	var oParent =document.getElementById('main');
+	var oBoxs =getByClass(oParent, 'box');
+	var lastBoxH = oBoxs[oBoxs.length-1].offsetTop+Math.floor(oBoxs[oBoxs.length-1].offsetHeight/2); 
+	var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+	// console.log(scrollTop);
+	var height = document.body.clientHeight || document.documentElement.clientHeight;
+	// console.log(height);
+	return (lastBoxH < scrollTop + height)?true : false;
 }

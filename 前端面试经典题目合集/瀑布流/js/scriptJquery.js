e262b499a7992caa108597dfea518e2689339859
youthcity <397731024@qq.com>
2016-06-11 23:46:28 +0800

    add jQuery实现瀑布流

---
+++ 前端面试经典题目合集/瀑布流/js/scriptJquery.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	$(window).on('load',function(){
+		waterfall();
+		var dataInt = {"data":[{"src":'26.jpg'},{"src":'27.jpg'},{"src":'28.jpg'},{"src":'29.jpg'},{"src":'30.jpg'},{"src":'31.jpg'},{"src":'32.jpg'},{"src":'33.jpg'},{"src":'34.jpg'},{"src":'35.jpg'},{"src":'36.jpg'}]};
+		$(window).on('scroll', function(){
+			if(checkScrollSlide()){
+				$.each(dataInt.data, function(key,value){
+					console.log(value);
+				});
+			}
+	});
+	});
+});
+
+
+function waterfall(){
+	var $boxs = $('#main>div');
+	var w = $boxs.eq(0).outerWidth();
+	var cols = Math.floor($(window).width()/w);
+	$('#main').width(w*cols).css('margin', '0 auto');
+	var hArr = [];
+	$boxs.each(function(index, value) {
+		var h = $boxs.eq(index).outerHeight();
+		if (index < cols) {
+			hArr[index] = h;
+		}else {
+			var minH = Math.min.apply(null, hArr);
+			var minIndex = $.inArray(minH, hArr);
+			// console.log(value);  DOM对象
+			$(value).css({
+				'position': 'absolute',
+				'top': minH+'px',
+				'left': minIndex*w+'px'
+			});
+			hArr[minIndex]= minH+$(value).outerHeight();
+			// hArr[minIndex] = minH+ $boxs.eq(index).outerHeight();
+		}
+	});
+	console.log(hArr);
+}
+
+function checkScrollSlide(){
+	var $lastBox  = $('#main>div').last();
+	var lastBoxDis = $lastBox.offset().top+Math.floor($lastBox.outerHeight()/2);
+	var scrollTop = $(window).scrollTop();
+	var documentH =$(window).height();
+
+	return lastBoxDis < scrollTop  + documentH;
+
+}
